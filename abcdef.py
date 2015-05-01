24872b8de2e76719a9c7e4653501bd336e26658d
Vishal Anand <anand.vishal2994@gmail.com>
2015-05-01 13:11:08 +0530

    Cleaned scripts

--- abcdef.py
+++
@@ -1,2 +0,0 @@
-for i in range(17,1272):
-	print i
--- execute.py
+++ execute.py
@@ -4,12 +4,13 @@ with requests.Session() as c:
 	headers={ 'content-type':'application/x-www-form-urlencoded; charset=UTF-8','Accept-Encoding': 'gzip, deflate','User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0','Referer' : 'http://www.zaffingo.com',
     }
 	url = "http://www.zaffingo.com/"
-	USERNAME = 'abcismadarchod@gmail.com'
-	PASSWORD = 'zaffazaffa'
+	USERNAME = 'samplemail@gmail.com'
+	PASSWORD = 'samplepassword'
 	c.get(url)
-	login_data = dict(l_email='abcismadarchod@gmail.com', l_password='zaffazaffa', next='/')
+	login_data = dict(l_email=USERNAME, l_password=PASSWORD, next='/')
 	c.post(url,data=login_data, headers={"Referer": "http://www.zaffingo.com"})
 	for i in range(17,1272):
+		print i
 		c.post(
 			url='http://www.zaffingo.com/ajax/add_testimonial.php',
 			data={
